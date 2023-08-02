b75dad409dbafc5b201c127725fc919be8d22c41
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-08-02 07:46:20 -0300

    Ajusts login

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Esqueceu sua senha? Sem problemas. Informe seu e-mail e enviaremos um novo link para você resetar sua senha.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Enviar e-mail com a nova senha') }}
             </x-primary-button>
         </div>
     </form>
