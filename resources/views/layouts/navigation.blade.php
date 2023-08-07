d891e1e09909c23ecdc9c03fe551ab5ea6a68c45
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-08-07 15:46:50 -0300

    Fix Nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,11 +11,7 @@
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Painel') }}
-                    </x-nav-link>
-                </div>
+           
                 <div class="hidden sm:flex sm:items-center sm:ml-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
