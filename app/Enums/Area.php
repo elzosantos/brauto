e57dc293261c758c00c55ce110f3acb155952982
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-08-08 01:18:15 -0300

    Creating Enums

---
+++ app/Enums/Area.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums;
+
+enum Area: string
+{
+    case A =  "Admnistrador";
+    case U = "Cliente";
+    case C = "Consultor";
+    case D = "Default";
+
+    public static function fromValue(string $name): string
+    {
+        foreach (self::cases() as $role) {
+            if ($name == $role->name) {
+                return $role->value;
+            }
+        }
+        throw new \ValueError("$name is not valid");
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Seja bem vindo!") }}
+                    {{ __('Seja bem vindo!') }}
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Você ainda não possui nenhum veículo cadastrado. Deseja adicionar algum?') }}
+                    <p> <a href=""><u>Clique aqui!</u></a></p>
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Você ainda não realizou nenhuma consultoria. Deseja adicionar alguma?') }}
+                    <p> <a href=""><u>Clique aqui!</u></a></p>
+                </div>
+
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __('Realize o seu primeiro registro do histórico do seu veículo.') }}
+                    <p> <a href=""><u>Clique aqui!</u></a></p>
                 </div>
             </div>
         </div>
