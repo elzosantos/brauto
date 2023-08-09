b74892fcfdb189a372bb886decb2093e349e9a32
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-08-09 15:38:45 -0300

    Name app

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-            <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex" style="color: white;">{{ config('app.name', 'Laravel') }}
+            <div class="space-x-8 sm:-my-px sm:ml-10 sm:flex" style="color: white; font-size: 4ch">{{ config('app.name', 'Laravel') }}
                 
             </div>
 
