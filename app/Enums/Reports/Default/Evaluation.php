5364214447b0ea585ff1656a08eebff258ada266
Elzo Santos <elzo.santos.sousa@gmail.com>
2023-08-29 06:57:23 -0300

    Result consultancy

---
+++ app/Enums/Reports/Default/Evaluation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Enums\Reports\Default;
+ 
+
+enum Evaluation:string
+{ 
+
+    case UM = '1';
+    case INATIVO = 2; 
+    public static function fromValue(int $status): string
+    {
+        foreach (self::cases() as $role) {
+            if ($status == $role->value) {
+                return $role->name;
+            }
+        }
+        throw new \ValueError("$status is not valid");
+    }
+}
+ 
